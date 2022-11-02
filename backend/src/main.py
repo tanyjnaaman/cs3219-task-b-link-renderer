b1b559387ca835a110103c44f64699196620aedd
tanyjnaaman <tannaaman@hotmail.com>
2022-11-02 16:54:28 +0800

    add sudo

--- backend/src/main.py
+++ backend/src/main.py
@@ -31,7 +31,7 @@ app.include_router(
 # sanity
 @app.get("/")
 def root():
-    return {"message": "Hello World from backend. s!"}
+    return {"message": "Hello World from backend. plss!"}
 
 if __name__ == "__main__":
 
