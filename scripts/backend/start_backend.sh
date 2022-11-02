6a47de60478c42815e6496572c11e8847e613c46
tanyjnaaman <tannaaman@hotmail.com>
2022-11-02 17:19:37 +0800

    detach

--- scripts/backend/start_backend.sh
+++ scripts/backend/start_backend.sh
@@ -2,4 +2,4 @@
 cd /home/ubuntu/cs3219-task-b-link-renderer/
 sudo docker stop $(sudo docker ps -a -q)
 sudo docker rm $(sudo docker ps -a -q)
-sudo docker-compose up --build backend
+sudo docker-compose up --build --detached backend 
