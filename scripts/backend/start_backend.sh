bb647ced2e41af371c5c30cfc1e661602b68e91b
tanyjnaaman <tannaaman@hotmail.com>
2022-11-02 14:09:46 +0800

    update script

--- scripts/backend/start_backend.sh
+++ scripts/backend/start_backend.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 cd /home/ubuntu/cs3219-task-b-link-renderer/
-docker-compose up --build backend
+sudo docker stop $(docker ps -a -q)
+sudo docker rm $(docker ps -a -q)
+sudo docker-compose up --build backend
