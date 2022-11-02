fcc1d90724eb5ae914bf50f8830762716a7aacfc
tanyjnaaman <tannaaman@hotmail.com>
2022-11-02 14:21:29 +0800

    Test

--- backend/src/main.py
+++ backend/src/main.py
@@ -31,7 +31,7 @@ app.include_router(
 # sanity
 @app.get("/")
 def root():
-    return {"message": "Hello World from backend.Test!"}
+    return {"message": "Hello World from backend. Another!"}
 
 if __name__ == "__main__":
 
