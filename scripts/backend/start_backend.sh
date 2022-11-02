e18facc2005135610f2e3455d668468b4e59b39f
tanyjnaaman <tannaaman@hotmail.com>
2022-11-02 16:50:49 +0800

    add sudo

--- scripts/backend/start_backend.sh
+++ scripts/backend/start_backend.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd /home/ubuntu/cs3219-task-b-link-renderer/
-sudo docker stop $(docker ps -a -q)
-sudo docker rm $(docker ps -a -q)
+sudo docker stop $(sudo docker ps -a -q)
+sudo docker rm $(sudo docker ps -a -q)
 sudo docker-compose up --build backend
